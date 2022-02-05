37f291e2b7fb21b737fe88bff5cc90c2901aeb07
Raphael Boidol <boidolr@users.noreply.github.com>
2022-02-05 15:48:42 +0100

    Give image sizes in human readable numbers

--- pre_commit_images/optimizer.py
+++ pre_commit_images/optimizer.py
@@ -7,6 +7,14 @@ from typing import Iterable
 def _optimize_images(
     images: Iterable[str], optimizer_fn: Callable[[Path], Path], threshold: int
 ) -> bool:
+    def bytes_to_readable(file_size: int) -> str:
+        if file_size / (1024 * 1024) > 1:
+            return f"{file_size / (1024 * 1024):.2f}Mb"
+        elif file_size / (1024) > 1:
+            return f"{(file_size/1024):.2f}Kb"
+        else:
+            return f"{file_size}b"
+
     def optimize_single_image(path: str) -> None:
         fp = Path(path)
         output = optimizer_fn(fp)
@@ -15,8 +23,10 @@ def _optimize_images(
         diff = original_size - output.stat().st_size
         if diff > threshold:
             output.replace(fp)
+            readable_diff = bytes_to_readable(diff)
+            readable_size = bytes_to_readable(original_size)
             print(
-                f"Optimized {path} by {diff} of {original_size} bytes ({diff/original_size:.2%})"
+                f"Optimized {path} by {readable_diff} of {readable_size} ({diff/original_size:.2%})"
             )
         else:
             output.unlink()
